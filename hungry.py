4c4541b4abdfd53075ce0a31db8aa4130d43a764
QapitaPay <109344841+QapitaPay@users.noreply.github.com>
2022-10-21 06:19:53 -0700

    replaced tire with y on line 2

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry = input("are you hungry?")
-if hungry == "yes" and hungry == "tire":
+if hungry == "yes" and hungry == "y":
     print("eat samosa")
     print("eat pizza")
     print("add burger")
