f95048560bfb2b2ab5b8f4b79d7021fdf413b0fe
Onyema Eze <ezeonyema47@gmail.com>
2022-10-21 03:35:35 -0700

    added tired

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry = input("are you hungry?")
-if hungry == "yes" and hungry == "yo":
+if hungry == "yes" and hungry == "tired":
     print("eat samosa")
     print("eat pizza")
     print("add burger")
